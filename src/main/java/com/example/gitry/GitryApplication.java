d0e3b504d801fd9a68aa7ec4577f616e105a82cb
erayfelek1@hotmal.com <erayfelekbusiness@hotmail.com>
2023-04-03 00:05:53 +0300

    init

---
+++ src/main/java/com/example/gitry/GitryApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitryApplication.class, args);
+    }
+
+}
